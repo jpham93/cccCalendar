1c7834daccbcdf0a78ab2000df1a678211071020
Dominic <dvphan@ucdavis.edu>
2019-03-02 13:26:30 -0800

    get and post routes complete

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,21 +1,33 @@
 const mongoose = require('mongoose')
 // const moment = require('moment')
 
-const { Schema } = mongoose 
+const { Schema } = mongoose
 
 const EventSchema = new Schema({
+  created: {
+    type: 'Moment',
+    required: true
+  },
   title: {
     type: String,
     required: true,
   },
-  date: {
-    type: Date,
+  start: {
+    type: 'Moment',
+    required: true,
+  },
+  end: {
+    type: 'Moment',
     required: true,
   },
   details: {
     type: String,
     required: false,
   },
+  url: {
+    type: String,
+    required: false,
+  }
 });
 
 const Event = mongoose.model('events', EventSchema);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const morgan = require('morgan');
+var mongoose = require('mongoose')
+require('mongoose-moment')(mongoose);
+const config = require('./db');
+const eventRoute = require('./routes/eventRoute');
+const app = express();
+const port = process.env.PORT || '3001';
 
-const app = express()
+
+mongoose.connect(config.DB, { useNewUrlParser: true })
+  .then(
+    () => { console.log('Database is connected'); },
+    err => console.log(`Can not connect to the database: ${err}`),
+  );
+
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/api/events', eventRoute);
+
+app.use((req, res) => {
+  res.status(404);
+  res.send('error 404');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  console.log(`ERROR: ${err}`);
+  res.send(`ERROR 500: ${err}`);
+});
+
+app.listen(port, () => {
+  console.log(`Express server started on ${port}`);
+});
